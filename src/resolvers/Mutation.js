48f3a82280d001eed425bdd52c6a69daebf42f22
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-10-16 15:18:42 +0530

    updateUser mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -47,6 +47,38 @@ const Mutation = {
         return deletedUsers[0];
 
     },
+    updateUser (parent, args, {db}, info) {
+        const {id, data} = args;
+
+        //find the user
+        const user = db.users.find((user) => user.id === id);
+
+        if(!user) {
+            throw new Error('user does not exist!');
+        }
+
+        if(typeof data.email === "string") {
+            //verify if no other user has the same email
+            const emailTaken = db.users.some((user) => user.email === data.email);
+
+            if(emailTaken) {
+                throw new Error('Email taken!');
+            }
+
+            user.email = data.email;
+        }
+
+        if(typeof data.name === "string") {
+            user.name = data.name;
+        }
+
+        if(typeof data.age !== "undefined") {
+            user.age = data.age;
+        }
+
+        return user;
+
+    },
     createPost (parent, args, {db}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author);
 
