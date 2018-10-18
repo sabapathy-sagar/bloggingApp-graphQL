7e27ea575e3dfa3fa3d91f6f56f48733ac763658
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-10-18 10:06:41 +0530

    count subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,4 +1,23 @@
 const Subscription = {
+    count: {
+        subscribe (parent, args, {pubsub}, info) {
+            let count = 0;
+
+            setInterval(() => {
+                count++
+                //publish method takes two arguments
+                //name of the channel - count
+                //data to publish
+                pubsub.publish('count', {
+                    count
+                })
+
+            }, 1000)
+
+            //set up the channel, with a name called 'count'
+            return pubsub.asyncIterator('count');
+        }
+    }
 
 };
 
