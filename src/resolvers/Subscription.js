b8c5307da9f2295155876c7d0e525bb227cab656
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-10-18 12:16:47 +0530

    remove count subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,23 +1,4 @@
 const Subscription = {
-    count: {
-        subscribe (parent, args, {pubsub}, info) {
-            let count = 0;
-
-            setInterval(() => {
-                count++
-                //publish method takes two arguments
-                //name of the channel - count
-                //data to publish - count data
-                pubsub.publish('count', {
-                    count
-                })
-
-            }, 1000)
-
-            //set up the channel, with a channel name called 'count'
-            return pubsub.asyncIterator('count');
-        }
-    },
     comment: {
         subscribe (parent, args, ctx, info) {
             const {postId} = args;
